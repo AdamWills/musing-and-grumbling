9a2d1dc6f6b168f3a34b87cf70f5712eb87ef4d2
Adam Wills <adam@adamwills.com>
2023-05-17 08:18:57 -0400

    initial setup

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,43 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import remarkEmbedder, { TransformerInfo } from "@remark-embedder/core";
+import oembedTransformer from "@remark-embedder/transformer-oembed";
+
+function handleHTML(html: string, info: TransformerInfo) {
+  const { url, transformer } = info;
+  if (
+    transformer.name === "@remark-embedder/transformer-oembed" ||
+    url.includes("youtube.com")
+  ) {
+    return `<div class="embed-youtube aspect-video">${html}</div>`;
+  }
+  return html;
+}
+
+const BandCampTransformer = {
+  name: "bandcamp",
+  shouldTransform(url: string) {
+    const { host } = new URL(url);
+    return host.endsWith(".bandcamp.com");
+  },
+  getHTML(url: string) {
+    return `<iframe title="BandCamp embed" style="border: 0; width: 100%; height: 120px;" src="https://bandcamp.com/EmbeddedPlayer/album=3745820516/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/artwork=small/transparent=true/" seamless><a href="${url}">${url}</a></iframe>`;
+  },
+};
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const result = await unified()
+    .use(remarkEmbedder, {
+      transformers: [oembedTransformer, BandCampTransformer],
+      handleHTML,
+    })
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .process(markdown);
+  return result.toString();
 }
